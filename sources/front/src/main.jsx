f98610d87713da0f16e61faef285c90144d7c5a0
Andrzej Sołtysik <svizz17@gmail.com>
2016-01-10 12:10:13 +0100

    webpack, npm and react configration for frontend

---
+++ sources/front/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Main extends React.Component {
+    render() {
+        return <h1>Foo</h1>
+    }
+}
+
+React.render(<Main/>, document.getElementById('app'));
---
+++ sources/front/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: [
+        './src/main.jsx'
+    ],
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+
+};
