d555b9f08c80407f22cda96706cb8b94dcb63ce4
ShakazMantaur <shakazunno@gmail.com>
2016-01-12 22:37:25 +0100

    Spring configuration

---
+++ sources/back/src/main/java/shizzie/ShizzieApplication.java
@@ -0,0 +1,12 @@
+package shizzie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShizzieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShizzieApplication.class, args);
+	}
+}
